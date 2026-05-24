09ccc3118107ad70c7dc1bd0d9737ed4052ea31f
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-05-23 19:02:25 -0600

    Swap strategies for GHDL

---
+++ create_ghdl_venv.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Create a virtual environment for GHDL
+
+script_dir="$(dirname -- "${BASH_SOURCE[0]:-$0}")" && readonly script_dir
+source "$script_dir/.env"
+
+readonly VENV_BIN_DIR=$script_dir/.venv_ghdl/bin
+mkdir -p "$VENV_BIN_DIR"
+
+# shellcheck disable=SC2016
+echo 'function deactivate_ghdl() {
+    # reset old environment variables
+    if [ -n "${_GHDL_OLD_VIRTUAL_PATH:-}" ] ; then
+        PATH="${_GHDL_OLD_VIRTUAL_PATH:-}"
+        export PATH
+        unset _GHDL_OLD_VIRTUAL_PATH
+    fi
+
+    # Call hash to forget past locations. Without forgetting
+    # past locations the $PATH changes we made may not be respected.
+    # See "man bash" for more details. hash is usually a builtin of your shell
+    hash -r 2> /dev/null
+
+    if [ ! "${1:-}" = "nondestructive" ] ; then
+        # Self destruct!
+        unset -f deactivate_ghdl
+    fi
+}
+
+deactivate_ghdl nondestructive
+
+_GHDL_OLD_VIRTUAL_PATH="$PATH"
+PATH="'"$VENV_BIN_DIR"'":"$PATH"
+export PATH
+
+hash -r 2> /dev/null' > "$VENV_BIN_DIR/activate"
+
+# shellcheck disable=SC2016
+echo '# shellcheck disable=SC2068
+podman run --rm -v "$PWD":/src -w /src ghdl/ghdl:'"$GHDL_TAG"' ghdl $@' > "$VENV_BIN_DIR/ghdl"
+chmod +x "$VENV_BIN_DIR/ghdl"
