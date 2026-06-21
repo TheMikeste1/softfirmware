ce47c1c31935cca1910c73a0c40d5fb1fc3386fc
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-06-21 14:54:41 -0600

    Complete VHDL Workshop Lab 04

--- vhdl_workshop/lab_4/disp_drv.vhd
+++ vhdl_workshop/lab_4/disp_drv.vhd
@@ -22,27 +22,31 @@ begin
     if i_error = '1' then
       w_disp_photo <= (10, 10, 10);
     elsif show_time = '0' then
-    -- output = picture count
+      -- output = picture count
+      w_disp_photo <= no_pics;
     else
-    -- output = exposure time
+      -- output = exposure time
+      w_disp_photo <= exp_time;
     end if;
   end process disp_mux;
 
   decode : process (w_disp_photo) is
   begin
     -- Process all elements of the data arrays
-    case w_disp_photo is
-      when 0 => display <= SEG_0;
-      when 1 => display <= SEG_1;
-      when 2 => display <= SEG_2;
-      when 3 => display <= SEG_3;
-      when 4 => display <= SEG_4;
-      when 5 => display <= SEG_5;
-      when 6 => display <= SEG_6;
-      when 7 => display <= SEG_7;
-      when 8 => display <= SEG_8;
-      when 9 => display <= SEG_9;
-      when others => display <= SEG_E;
-    end case;
+    for i in w_disp_photo'range loop
+      case w_disp_photo(i) is
+        when 0 => display(i) <= SEG_0;
+        when 1 => display(i) <= SEG_1;
+        when 2 => display(i) <= SEG_2;
+        when 3 => display(i) <= SEG_3;
+        when 4 => display(i) <= SEG_4;
+        when 5 => display(i) <= SEG_5;
+        when 6 => display(i) <= SEG_6;
+        when 7 => display(i) <= SEG_7;
+        when 8 => display(i) <= SEG_8;
+        when 9 => display(i) <= SEG_9;
+        when others => display(i) <= SEG_E;
+      end case;
+    end loop;
   end process decode;
 end architecture rtl;
