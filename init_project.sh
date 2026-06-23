2f0842a9031eac99bf91ade8d9c6536a959d9ce3
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-06-22 20:48:15 -0600

    Fix activate script

--- init_project.sh
+++ init_project.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-script_dir="$(dirname -- "${BASH_SOURCE[0]:-$0}")" && readonly script_dir
+script_dir="$(realpath "$(dirname -- "${BASH_SOURCE[0]:-$0}")")" && readonly script_dir
 cd "$script_dir" || exit 1
 
 python3 -m venv .venv
@@ -8,3 +8,13 @@ python3 -m pip install -r requirements.txt
 python3 ./generate_fusesoc_schema.py
 
 ./create_ghdl_venv.sh
+
+# shellcheck disable=SC2016
+echo '#!/usr/bin/env bash
+if ! (return 0 2>/dev/null); then
+  echo "Error: This script must be sourced. Run: . $0" >&2
+  exit 1
+fi
+
+. '"$script_dir/.venv_ghdl/bin/activate"'
+. '"$script_dir/.venv/bin/activate" > "$script_dir/activate"
