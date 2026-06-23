436927344e0fe9dcec4cadd347586bdd90500798
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-06-23 13:39:13 -0600

    Fix fusesoc schema

--- generate_fusesoc_schema.py
+++ generate_fusesoc_schema.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 from pathlib import Path
 
 from fusesoc.capi2.json_schema import capi2_schema
@@ -7,8 +8,14 @@ from fusesoc.capi2.json_schema import capi2_schema
 SCRIPT_DIR = Path(__file__).parent.resolve()
 
 def main():
+    schema = json.loads(capi2_schema.strip())
+    schema["properties"]["CAPI=2"] = {
+      "description": "Specifies the version of CAPI",
+      "type": "null"
+    }
+
     with open(SCRIPT_DIR / "fusesoc_schema.json", "w", encoding="utf-8") as fout:
-        fout.write(capi2_schema.strip())
+        json.dump(schema, fout, indent=2)
 
 if __name__ == "__main__":
     main()
