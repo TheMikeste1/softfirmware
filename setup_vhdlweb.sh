5816a131638a4398a36dc19d32ce819c7011aac2
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-06-27 12:19:58 -0600

    Set up for VHDL web

---
+++ setup_vhdlweb.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+script_dir="$(realpath "$(dirname -- "${BASH_SOURCE[0]:-$0}")")" && readonly script_dir
+if [[ ! -f "$script_dir/vhdlweb/template.config" ]]; then
+  echo "Could not find vhdlweb. Has the submodule been pulled?" >%2
+  exit 1
+fi
+
+cp "$script_dir/vhdlweb/template.config" "$script_dir/vhdlweb/deploy.config"
+sed "s/SECRET_KEY = b''/SECRET_KEY = b'$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 16; echo "")'/" "$script_dir/vhdlweb/deploy.config" -i
+sed "s%SRCDIR=\"/home/vhdlweb/vhdlweb/data/problems\"%SRCDIR=\"$script_dir/vhdlweb/data/problems\"%" "$script_dir/vhdlweb/deploy.config" -i
+docker_image="$(grep DOCKER_IMAGE "$script_dir/vhdlweb/deploy.config")"
+docker_image="${docker_image#*\"}"
+docker_image="${docker_image%\"}"
+readonly docker_image
+docker pull "$docker_image"
+
+npm install -g netlistsvg # For diagrams
+
+if command -v getenforce &> /dev/null; then
+  echo "Running SELinux, adding modules. . ."
+  cd "$script_dir" || {
+    echo "Failed to add modules" >&2
+    exit 1
+  }
+
+  checkmodule -M -m -o vhdlweb-make.mod vhdlweb-make.te
+  semodule_package -o vhdlweb-make.pp -m vhdlweb-make.mod
+
+  checkmodule -M -m -o vhdlweb-ghdl.mod vhdlweb-ghdl.te
+  semodule_package -o vhdlweb-ghdl.pp -m vhdlweb-ghdl.mod
+
+  sudo semodule -X 300 -i vhdlweb-make.pp
+  sudo semodule -X 300 -i vhdlweb-ghdl.pp
+
+  rm vhdlweb-ghdl.mod vhdlweb-make.mod vhdlweb-ghdl.pp vhdlweb-make.pp
+fi
+
+echo "VHDL web ready; run $script_dir/vhdlweb/deploy"
---
+++ uninstall_vhdlweb.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if command -v getenforce &> /dev/null; then
+  echo "Running SELinux, removing modules. . ."
+  sudo semodule -X 300 -r vhdlweb-ghdl
+  sudo semodule -X 300 -r vhdlweb-make
+fi
